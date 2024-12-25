2924d8d65bd1dfefd837efef61938f8e29350ebe
Angel Zolotarev <fuzisamprp09@gmail.com>
2024-12-25 17:35:17 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/angelzolotarev/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/angelzolotarev/', // Укажите имя вашего репозитория
 });
